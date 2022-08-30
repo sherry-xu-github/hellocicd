489a43e80513d0bd1f88bdbe66c8c44a93e78b9c
Sherry Xu <sherryx2020@gmail.com>
2022-08-30 21:50:54 +0000

    adding everything else

---
+++ src/main/java/com/example/hellocicd/HellocicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellocicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellocicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellocicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hellocicd/HellocicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellocicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellocicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
